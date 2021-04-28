da24116d6514ceb92ddcbd3a95bcbf78cef5b1f7
Vincent <vleclerc@inow.fr>
2021-04-28 18:00:08 +0200

    unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { InputPassword } from './core/components';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test password', () => {
+  render(<InputPassword name='bob' />);
+  const input = screen.getByTestId(/password/i);
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveValue('');
+});
+
+
+
